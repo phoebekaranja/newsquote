c2a91e5157f3e0a3f348372f59509d6b46e800fa
phoebekaranja <phoebemurish@gmail.com>
2018-10-18 12:15:06 +0300

    created a pipe and made some changes to some files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { QuotesComponent } from './quotes/quotes.component';
 import { HighlightDirective } from './highlight.directive';
+import { DateTimePipe } from './date-time.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     QuotesComponent,
-    HighlightDirective
+    HighlightDirective,
+    DateTimePipe
   ],
   imports: [
     BrowserModule
--- src/app/highlight.directive.ts
+++ src/app/highlight.directive.ts
@@ -1,10 +1,20 @@
-import { Directive } from '@angular/core';
-
+import { Directive,Input,ElementRef,HostListener } from '@angular/core';
 @Directive({
   selector: '[appHighlight]'
 })
 export class HighlightDirective {
+    @Input('appHighlight') highest:boolean;
 
-  constructor() { }
+  constructor(private elem:ElementRef) {
+  }
+    @HostListener('change') ngOnChanges(){
+        if(this.highest){
+            this.elem.nativeElement.style.backgroundColor='#222';
+            this.elem.nativeElement.style.color='#fff';
+        }else{
+            this.elem.nativeElement.style.backgroundColor='#fff';
+            this.elem.nativeElement.style.color='#000';
+        }
+    }
 
 }
