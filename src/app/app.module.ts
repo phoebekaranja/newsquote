ff75421a0acd7c728d383604f61559f3d680ebb7
phoebekaranja <phoebemurish@gmail.com>
2018-10-18 12:38:12 +0300

    added some changes to some files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { QuotesComponent } from './quotes/quotes.component';
-import { HighlightDirective } from './highlight.directive';
-import { DateTimePipe } from './date-time.pipe';
+import { QuoteComponent } from './quote/quote.component';
+import { QuoteDetailsComponent } from './quote-details/quote-details.component';
 import { QuoteFormComponent } from './quote-form/quote-form.component';
+import { TimerPipe } from './timer.pipe';
+import { HighlightDirective } from './highlight.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
-    QuotesComponent,
-    HighlightDirective,
-    DateTimePipe,
-    QuoteFormComponent
+    QuoteComponent,
+    QuoteDetailsComponent,
+    QuoteFormComponent,
+    TimerPipe,
+    HighlightDirective
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/quote-form/quote-form.component.ts
+++ src/app/quote-form/quote-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-quote-form',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quote-form.component.css']
 })
 export class QuoteFormComponent implements OnInit {
+
+    @Input () addQuote: Function;
 
   constructor() { }
 
